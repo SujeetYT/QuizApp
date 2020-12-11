0c91e966144a4a287874e93b354269e06d4d47ab
SujeetYT <sujeet7982422191@gmail.com>
2020-12-11 23:49:53 +0530

    Some Changes

---
+++ JavaScript/QuizApp.js
@@ -0,0 +1,6 @@
+// This is a JavaScript File for the QuizApp
+function submitForm(e) {
+    e.preventDefault();
+    var candidateName = document.getElementById("name").value;
+    console.log(candidateName);
+}
--- QuizApp.js
+++
@@ -1,1 +0,0 @@
-// This is a JavaScript File for the QuizApp
